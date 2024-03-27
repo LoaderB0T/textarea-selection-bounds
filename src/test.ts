0dfe647398b56f82ab286da82cea7fafd7260587
Janik Schumacher <rndmb0t@gmail.com>
2024-03-27 16:08:32 +0100

    fixes

--- src/test.ts
+++
@@ -1,7 +0,0 @@
-import { TextareaSelectionBounds } from './textarea-selection-bounds';
-
-const textArea = document.querySelector('textarea');
-if (textArea) {
-  const bounds = new TextareaSelectionBounds(textArea);
-  console.log(bounds.getBounds());
-}
